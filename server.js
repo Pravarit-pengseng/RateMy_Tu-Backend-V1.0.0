c7ac79af6e5fe58b7469e75d15b6e7ab81208282
Pravarit-pengseng <pravarit947@gmail.com>
2025-10-13 23:08:44 +0700

    fix for profile
    by pond

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const bodyParse = require("body-parser");
 const { readdirSync } = require("fs");
 const bodyParser = require("body-parser");
 const connectDB = require("./Config/db");
-
-
+const path = require("path");
 
 const app = express();
 
@@ -16,7 +15,7 @@ connectDB();
 app.use(morgan("dev"));
 app.use(cors());
 app.use(bodyParse.json({ limit: "10mb" }));
-
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 readdirSync("./Routes").map((r) => app.use("/api", require("./Routes/" + r)));
 const PORT = process.env.PORT || 5000;
